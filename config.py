77e8a6b2d6623c7199b62f7856687bfc90cf282d
fastcontract <106406012+fastcontract@users.noreply.github.com>
2022-10-06 10:43:13 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 VALIDATORS = [X,X,X,X,X,X]
-NOTIFY_ON_SEQ_MISSES = 3
 BOT_TOKEN = ""
 CHAT_ID = XXXXXXXXXX
 BEACON_API = ""
+
+#I recommend setting the notify threshold higher than 2.
+#Sometimes the beacon API doesnt update fast enough on a new epoch and marks a validator as missed.
+#It then later updates it the next epoch when it was a successful attestation.
+#That is why the beaconchain website actually lags 1 epoch behind when displaying attestations.
+NOTIFY_ON_SEQ_MISSES = 3
